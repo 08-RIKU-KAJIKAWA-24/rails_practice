4c2fe7e833d269c2919ca5edd996c6098a8748de
Riku Kajikawa <rikukajikawa@RikuMacBookAir.local>
2020-07-15 23:44:54 +0900

    first rails

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def top
+    @comment = "Welcome to Riku's Bank!"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get '/' => "home#top"
+  get 'about' => 'home#about'
+  get 'posts/index' => 'posts#index'
+  get 'posts/show' => 'posts#show'
+  get 'posts/new' => 'posts#new'
+  post 'posts/create' => 'posts#create'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
